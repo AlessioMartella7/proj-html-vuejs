35d372e05087c992e9e4d4a7e730a641e7d386a3
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-09-25 22:37:38 +0200

    Imported logos / bootstrap on general.scss

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,15 +2,27 @@
 export default {
   data() {
     return {
-      
+
     }
   }
 }
 </script>
 
 <template>
-<p>Footer</p>
+  <p>Footer</p>
+  <div>
+    <div class="container w-100 ">
+      <!-- Contatti -->
+      <div id="footer-contacts">
+        <img src="" alt="">
+      </div>
+      <!-- Form -->
+      <!-- lista Trainings -->
+      <!-- lista Quick Links -->
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use "../styles/general.scss" as *;
 </style>
