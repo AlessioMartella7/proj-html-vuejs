2952efa85b375485024a355a67b31b1725a282f4
FrancescoSisti <francescosisti61@gmail.com>
2024-09-25 22:52:44 +0200

    initial sidebar structure

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,19 +1,70 @@
 <template>
     <div class="sidebar">
-        <h1>Sidebar</h1>
+        <div class="sidebar-header">
+            <img src="../assets/sidebar-assets/loader-gobike.png" alt="logo">
+        </div>
+        <div class="sidebar-body">
+            <h1><strong>Contact Information</strong></h1>
+            <font-awesome-icon :icon="['fas', 'phone']" />
+            <font-awesome-icon :icon="['fas', 'envelope']" />
+        </div>
     </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPhone, faEnvelope)
+
 export default {
-    name: 'Sidebar'
+    name: 'Sidebar',
+    components: {
+        FontAwesomeIcon
+    },
+    data() {
+        return {
+            isOpen: false
+        }
+    }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+}
+
 .sidebar {
-    width: 200px;
+    width: 400px;
     height: 100vh;
-    background-color: #f0f0f0;
+    background-color: #fff;
+    position: fixed;
+    top: 0;
+    left: 0;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.sidebar-header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 60px;
+}
+
+.sidebar-body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+}
+
+h1 {
+    padding-top: 130px;
 }
 </style>
