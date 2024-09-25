0f6cc3b65c0dbf0cdb4a8fef7822093864a6b7fe
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-09-25 22:58:38 +0200

    Added contacts section

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -10,19 +10,36 @@ export default {
 
 <template>
   <p>Footer</p>
-  <div>
-    <div class="container w-100 ">
-      <!-- Contatti -->
-      <div id="footer-contacts">
-        <img src="" alt="">
+  <footer class="w-100">
+    <div class="container">
+      <div class="row">
+        <div class="col-3">
+          <!-- Contatti -->
+          <div id="footer-contacts">
+            <img src="../assets/logos/logo-footer-gobike.png" alt="footer_gobike_logo" class="img-fluid">
+
+            <!-- Contacts comp -->
+            <!-- Social comp -->
+          </div>
+        </div>
+        <!-- Form -->
+        <!-- lista Trainings -->
+        <!-- lista Quick Links -->
+
       </div>
-      <!-- Form -->
-      <!-- lista Trainings -->
-      <!-- lista Quick Links -->
     </div>
-  </div>
+    <!-- Rights Reserved -->
+    <div>
+      <p>2024 Gobike - All rights reserved.</p>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
 @use "../styles/general.scss" as *;
+
+footer {
+  background-color: black;
+  color: white;
+}
 </style>
