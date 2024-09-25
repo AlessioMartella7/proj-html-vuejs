2d4a07aa626f2dd50c3c2fc6fa6b3c5102a71444
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-09-26 00:05:17 +0200

    Completed contacts

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,12 @@
 <script>
+import { contactInfo } from '../data/contactInfo';
+import { socialIcons } from '../data/contactInfo';
+
 export default {
   data() {
     return {
+      contactInfo,
+      socialIcons,
 
     }
   }
@@ -19,7 +24,20 @@ export default {
             <img src="../assets/logos/logo-footer-gobike.png" alt="footer_gobike_logo" class="img-fluid">
 
             <!-- Contacts comp -->
+            <ul class="m2">
+              <li class="list-unstyled mb-4" v-for="(item, index) in contactInfo" :key="index">
+                <font-awesome-icon :icon="item.icon" class="icon fs-4 me-4" />
+                <span>{{ item.info }}</span>
+              </li>
+            </ul>
             <!-- Social comp -->
+            <div class="social d-flex justify-content-around row">
+              <div class="social-icon col-3 p-2 text-center" v-for="(item, index) in socialIcons" :key="index">
+                <a><font-awesome-icon :icon="item.icon" /></a>
+
+              </div>
+
+            </div>
           </div>
         </div>
         <!-- Form -->
@@ -30,7 +48,7 @@ export default {
     </div>
     <!-- Rights Reserved -->
     <div>
-      <p>2024 Gobike - All rights reserved.</p>
+      <p class="text-center my-4">2024 Gobike - All rights reserved.</p>
     </div>
   </footer>
 </template>
@@ -41,5 +59,14 @@ export default {
 footer {
   background-color: black;
   color: white;
+}
+
+ul {
+  padding: 0;
+}
+
+.social-icon {
+  border: 1px solid gray;
+  cursor: pointer;
 }
 </style>
