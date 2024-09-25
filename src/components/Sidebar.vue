26bb0da8d1b48ce58c735421c55f6cd3e549f70d
FrancescoSisti <francescosisti61@gmail.com>
2024-09-25 22:08:51 +0200

    ticketing commit

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="sidebar">
+        <h1>Sidebar</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Sidebar'
+}
+</script>
+
+<style lang="scss">
+.sidebar {
+    width: 200px;
+    height: 100vh;
+    background-color: #f0f0f0;
+}
+</style>
