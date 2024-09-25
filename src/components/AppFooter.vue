2cbe8477399e67e112cd15d32a5ee51dc8fd70e0
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-25 17:53:39 +0200

    first setup

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+<p>Footer</p>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+<p>Header</p>
+</template>
+
+<style scoped>
+</style>
