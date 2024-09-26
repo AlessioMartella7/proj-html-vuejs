2c8767f18e4533e3bc19418abc3eb8cfed630117
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-26 15:32:07 +0200

    added text formattation

--- src/components/AppMainAboutUs.vue
+++ src/components/AppMainAboutUs.vue
@@ -13,16 +13,16 @@ export default {
     <div class="row">
 
         <!-- left side -->
-        <div class="col-8">
+        <div class="col-7">
             <img id="bikers" src=".././assets/img_aboutus/bike-player.png" alt="player-img">
                 <img id="gear" class="rotate" src="../assets/img_aboutus/bike-circle.png" alt="gear">
         </div>
 
         <!-- right side -->
-        <div class="col-4 d-flex flex-column justify-content-center align-items-start">
+        <div class="col-5 d-flex flex-column justify-content-center align-items-start">
             <div class=" top-text">
-                <h3>Do you want to be a professional cyclist?</h3>
-                <p>We offer 6 courses of varying difficulty and beautiful scenary that cyclist of all levels can enjoy it. You will learn cycling from professionals
+                <h3 class="fw-bold">Do you want to be a professional cyclist?</h3>
+                <p class="mb-1">We offer 6 courses of varying difficulty and beautiful scenary that cyclist of all levels can enjoy it. You will learn cycling from professionals
                     with our component and experienced staff. You will have a lot of fun with our amazing spaces.
                 </p>
              </div>
@@ -36,7 +36,7 @@ export default {
                 <!-- professional team -->
                 <div class="team d-flex align-items-center">
                     <img class="img-fluid" src="../assets/img_icons/f1-helmet-svgrepo-com.svg" alt="race-helmet">
-                    <div class="text">
+                    <div class="text ms-3 pt-4">
                         <h4 class="fw-bold">Professional Team</h4>
                         <p>We make you professional by adding your sincerity as well as professionalism.</p>
                     </div>
@@ -45,14 +45,14 @@ export default {
                 <!-- professional trainings -->
                 <div class="trainings d-flex align-items-center">
                     <img class="img-fluid" src="../assets/img_icons/helmet-svgrepo-com.svg" alt="helmet">
-                    <div class="text">
+                    <div class="text ms-3">
                         <h4 class="fw-bold">Professional trainings</h4>
                         <p>We always ride with the best equipment, respecting nature and fun.</p>
                     </div>
                 </div>
 
                 <!-- button -->
-                <button type="button" class="btn btn-dark btn-lg px-5 pt-3 pb-3 rounded-0 text-white fs-6">Get to know us &RightArrow;</button>
+                <button type="button" class="btn btn-dark btn-lg px-5 pt-3 pb-3 rounded-0 text-white fs-6 my-4 fw-bold">Get to know us &RightArrow;</button>
              </div>
 
         </div>
