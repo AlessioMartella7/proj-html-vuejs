57ff0a2763c2d8a949b9ddbc49bf18fbad2233a0
FrancescoSisti <francescosisti61@gmail.com>
2024-09-26 15:26:39 +0200

    added mouse follow logic

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,50 @@
 <script>
 export default {
-  data() {
-    return {
-      
+  name: 'MouseFollow',
+  mounted() {
+    document.addEventListener('mousemove', this.handleMouseMove);
+  },
+  beforeUnmount() {
+    document.removeEventListener('mousemove', this.handleMouseMove);
+  },
+  methods: {
+    handleMouseMove(event) {
+      const mouseFollow = this.$refs.mouseFollow;
+      mouseFollow.style.left = `${event.clientX}px`;
+      mouseFollow.style.top = `${event.clientY}px`;
     }
   }
 }
 </script>
 
 <template>
-<p>Main</p>
+  <p>Main</p>
+  <div ref="mouseFollow" class="mouse-follow">
+    <div class="mouse-point"></div>
+  </div>
 </template>
 
-<style scoped>
+<style>
+.mouse-follow {
+  position: fixed;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 2px solid grey;
+  background-color: transparent;
+  pointer-events: none;
+  z-index: 9999;
+  transform: translate(-50%, -50%);
+}
+
+.mouse-point {
+  position: absolute;
+  width: 4px;
+  height: 4px;
+  background-color: grey;
+  border-radius: 50%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
