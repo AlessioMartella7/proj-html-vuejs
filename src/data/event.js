395b7154584e688ada1170c1974175d0fdfe24db
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-26 17:25:38 +0200

    created event.js

---
+++ src/data/event.js
@@ -0,0 +1,75 @@
+const event = [
+    {
+      img:'../assets/img_events/event1.jpg' , 
+      race: 'Toronto Cycling Event 2023',
+      date: '2022-06-22 @ 01.30 - 2024-08-23 @ 07:00 ',
+      location: 'Toronto',
+      type: 'Bycicle Race',
+    },
+     {
+      img:'../assets/img_events/event2.jpg' , 
+      race: 'Instanbul Cycling Event 2023',
+      date: '2022-06-30 @ 01.30 - 2023-08-17 @ 03:30 ',
+      location: 'Instabul',
+      type: 'Bike Sport',
+    },
+     {
+      img:'../assets/img_events/event3.jpg' , 
+      race: 'Paris Cycling Event 2023',
+      date: '2022-06-22 @ 07.30 - 2023-04-14 @ 16:30 ',
+      location: 'Paris',
+      type: 'Cycling Tour',
+    },
+     {
+      img:'../assets/img_events/event4.jpg' , 
+      race: 'New jersey Cycling Event 2023',
+      date: '2022-06-25 @ 03.30 - 2023-04-14 @ 05:30 ',
+      location: 'New Jersey',
+      type: 'Bycicle Race',
+    },
+     {
+      img:'../assets/img_events/event5.jpg' , 
+      race: 'London Cycling Event 2023',
+      date: '2022-06-28 @ 07.30 - 2023-03-18 @ 12:00 ',
+      location: 'London',
+      type: 'Bike Sport',
+    },
+     {
+      img:'../assets/img_events/event6.jpg' , 
+      race: 'Kiev Cycling Event 2023',
+      date: '2022-06-29 @ 10.00 - 2023-04-07 @ 04:30 ',
+      location: 'Kiev',
+      type: 'Bycicle Race',
+    },
+     {
+      img:'../assets/img_events/event7.jpg' , 
+      race: 'Colorado Cycling Event 2023',
+      date: '2022-06-28 @ 04.00 - 2023-03-24 @ 05:30 ',
+      location: 'Colorado',
+      type: 'Bycicle Race',
+    },
+     {
+      img:'../assets/img_events/event8.jpg' , 
+      race: 'Amsterdam Cycling Event 2023',
+      date: '2022-06-28 @ 10.30 - 2024-03-11 @ 10:30 ',
+      location: 'Amsterdam',
+      type: 'Bycicle Race',
+    },
+     {
+      img:'../assets/img_events/event9.jpg' , 
+      race: 'Toronto Cycling Event 2023',
+      date: '2022-06-22 @ 09.30 - 2023-04-23 @ 17:30 ',
+      location: 'Toronto',
+      type: 'Bycicle Race',
+    },
+     {
+      img:'../assets/img_events/event10.jpg' , 
+      race: 'Toronto Cycling Event 2023',
+      date: '2022-06-26 @ 05.30 - 2023-04-22 @ 14:00 ',
+      location: 'Paris',
+      type: 'Bycicle Race',
+    },
+    
+]
+
+export { event};
