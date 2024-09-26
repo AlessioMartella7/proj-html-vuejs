8e93d5ef9aeb6c66cfe1ca8e1bf274021272e643
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-09-26 14:21:19 +0200

    toggle function tryout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,27 +13,38 @@ export default {
       { text: 'Home', link: '#' },
         { text: 'About Us', link: '#' },
         { text: 'Training', link: '#',
-      /*  hoverMenu:[
+      hoverMenu:[
         {text: 'Riding Lessons', link: '#'},
         {text: 'Safe Driving', link:'#'},
         {text:'Mountain Bike', link:'#'},
         {text:'Trail Drive', link: '#'},
         {text:'Pedaling', link:'#'},
        {text:'All Trainings', link:'#'}
-        ]  */
+        ],
+       
     },
+  
         { text: 'Packages', link: '#' },
         { text: 'Blog', link: '#' },
         { text: 'Contact', link: '#' }
-      
-      
-      
-      ],
- 
-        }
-    },
+      ], isSidebarDisplayed: false,  // Stato di default di visivilità della sidebar
+
+       
+      }
+     }, methods:{
+        sidebarToggle(){     //funzione toggle che alterna true e false della visibilità della sidebar 
+     this.isSidebarDisplayed = !this.isSidebarDisplayed
+        } 
+      }
+      }
+
+
    
-}
+
+     
+     
+
+
 </script>
 
 <template>
@@ -43,23 +54,21 @@ export default {
   <li class="ms-3" v-for="(item, index) in menuItems" :key="index"> <!--Popolazione dinamica dell'header tramite array menuItems-->
     <a :href="item.link">{{ item.text }}</a>
    <!--Hover Sublist-->
-    <ul v-if="item.hoverMenu" class="sublist">
-                <li v-for="(hoverListItem, hoverListIndex) in item.hoverMenu" :key="hoverListIndex">
-                    <a :href="hoverListItem.link">{{ hoverListItem.text }}</a>
-                </li>
-    </ul>
+   
 
 
   </li>
 </ul>
  <div class="d-flex align-items-center justify-content-between gap-5">
-<a href="#" class="text-dark"><font-awesome-icon class="fs-3" :icon="['fas', 'bars']" /></a>
+<a href="#" @click.prevent="sidebarToggle" class="text-dark"><font-awesome-icon class="fs-3" :icon="['fas', 'bars']" /></a>
  <button class="btn btn-dark rounded">
    <img src="../assets/img_icons/f1-helmet-svgrepo-com.svg"  class="helmet">Upcoming Events <font-awesome-icon :icon="['fas', 'arrow-right']" />
  </button>
  </div>
-
+<p v-if="isSidebarDisplayed">toggle funcion tryout</p>
 </header>
+
+
 </template>
 
 <style lang="scss">
@@ -74,7 +83,7 @@ header{
     left: 0;
     width: 100%;
     font-family: sans-serif;
-    z-index:2;
+    z-index:1;
 padding: 0 50px;
 
 }
