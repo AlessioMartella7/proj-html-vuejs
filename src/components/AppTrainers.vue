0abdbc9757824cd4aebe06440bafe34584414a26
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-09-26 22:08:42 +0200

    adjusment to trainers layout system

--- src/components/AppTrainers.vue
+++ src/components/AppTrainers.vue
@@ -19,53 +19,46 @@ export default {
 <h2 class="text-center"><b>Our Expert Trainers</b></h2>
 <p class="text-center">Learn to ride a bike from the experts.</p>
 <!--Card section-->
- <div class="container ">
-  <div class="row mt-5">
-   <div class="col">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quae nisi commodi dolorum, voluptates ipsam neque voluptatum animi cum recusandae, deleniti velit, sunt provident illum quod nihil! Id, est sed.
-   Veniam maxime perspiciatis voluptates quaerat cum laborum accusantium sit explicabo quam deleniti modi rerum est tenetur excepturi nostrum repudiandae qui, odio velit? Sunt qui doloribus nobis, et recusandae reprehenderit rerum!
-   Voluptas, sint consequatur. Unde veritatis saepe odio ipsum, velit alias? Ex necessitatibus veniam neque quae adipisci, error, beatae officia similique vitae voluptate est, in illo cumque voluptatem aperiam hic iste.
-   Animi, cumque tempora consectetur, adipisci nulla mollitia quod quam vel iusto ex sed. Quo consequuntur nulla, praesentium non repellendus ab rerum est, minima corrupti ipsam eaque sed voluptates maxime neque?
-   Perspiciatis vero esse odit aspernatur harum cum quos rerum, magnam corporis, cumque laudantium a ex suscipit mollitia dolorum recusandae. Dicta porro hic a nam expedita, unde aliquid quos reiciendis rerum!
-   Sunt quo quam perferendis quae quidem, tempore beatae doloribus dolore eaque sed facere illo totam asperiores recusandae atque ipsam odio veritatis nemo impedit numquam omnis quisquam incidunt! Voluptatibus, similique repellat.
-   Mollitia nostrum debitis necessitatibus veniam ea suscipit dolorem, pariatur quaerat, deleniti magni possimus eveniet neque inventore aliquid optio rem explicabo eius voluptatem impedit quibusdam fugiat? Eius nam impedit magni ab.
-   Modi commodi architecto hic fugiat quo? Cumque, porro? Quam, iure porro dolore nobis recusandae ipsam blanditiis tempora hic voluptates ea, fugit nihil omnis saepe numquam, cum voluptas facere maxime earum.
-   Nam quia ipsam, ipsum, nostrum impedit beatae consequatur atque laboriosam recusandae perspiciatis unde, voluptatem deleniti ad. At neque natus eligendi. Quisquam, atque a adipisci ullam ipsa ea quae nam id!
-   Perspiciatis debitis nisi voluptatum architecto unde! Officia maxime quasi impedit mollitia beatae expedita delectus, rerum nisi tenetur placeat vitae possimus natus, debitis dolorum at, ipsam sequi numquam. Amet, quidem atque.</div>
-   <div class="col">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi assumenda exercitationem fuga recusandae aperiam, sit asperiores quisquam distinctio quibusdam architecto alias hic earum, consequatur incidunt illo neque. Nihil, non ullam.
-   Illum, hic molestias ab quibusdam libero non ratione, voluptatum voluptates, aperiam quo iure ipsum similique necessitatibus! Maxime eos cum officia fuga, eaque nemo delectus omnis dolorem at repellat in iste?
-   Eligendi voluptatibus voluptatem vel molestiae nesciunt, quos dicta nam, eveniet ducimus magni, at accusantium consequatur nobis. Amet sapiente, voluptatum modi nulla quo qui? Nemo impedit, ratione dolores aspernatur animi nulla.
-   Maxime, inventore corporis consectetur excepturi fuga recusandae repudiandae saepe magni praesentium fugit, maiores modi voluptates nulla nesciunt harum nostrum ratione iusto atque obcaecati, aut earum nam sequi repellat perferendis. Eum.
-   Incidunt repellat totam in enim cumque repellendus nobis quaerat cupiditate nulla dignissimos sed odio, quibusdam commodi repudiandae optio eveniet iste eligendi dolores voluptates consequatur temporibus. Expedita nam laborum ut eius?
-   Sit mollitia voluptas aliquid sed doloribus velit et illum harum ut? Rem quaerat quia porro? Nulla quod temporibus ipsam officia, totam repudiandae, est dignissimos veniam numquam dolore eligendi vel iste.
-   Incidunt eos quae, id facere, culpa veritatis impedit ducimus perspiciatis asperiores odio magni voluptatem quas excepturi eaque esse itaque cum consequatur quisquam nesciunt exercitationem harum? Ratione repudiandae amet tempora voluptatem?
-   Numquam, libero est, placeat velit totam aperiam possimus a rerum ad ex cupiditate, voluptates reiciendis impedit hic eligendi at tempora beatae iure facere necessitatibus! Recusandae deserunt aspernatur cupiditate voluptatum odit!
-   Dignissimos cumque dicta illo similique ipsum incidunt, earum, in ducimus, exercitationem impedit laborum maiores atque? Nostrum corporis iusto corrupti dolore expedita laborum labore, nihil sint vel. Labore odit harum recusandae!
-   Officiis sunt modi earum neque cumque inventore fugit necessitatibus voluptate dolorem consequatur, adipisci suscipit hic accusantium fugiat maiores nulla sint dolore omnis quod quos aliquam temporibus a placeat libero. Cupiditate.</div>
-   <div class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio facere odit officia eius. Dolores impedit sunt deleniti quasi? Dicta esse sit recusandae aliquam animi non mollitia accusantium molestiae est harum?
-   Eius, soluta reprehenderit dolorum quasi nostrum nesciunt accusamus incidunt debitis error id ducimus expedita delectus, consequatur, minus nemo! Praesentium perferendis beatae maiores libero saepe ipsam cumque dicta odit quisquam repellat?
-   Ipsam perspiciatis aspernatur incidunt. Modi facere minima, numquam dolorum fugiat atque asperiores dolor veritatis nam quisquam! Optio, quisquam. Fugiat voluptates alias minus quaerat ullam et quisquam, assumenda quia distinctio vero.
-   Ullam esse explicabo rem asperiores, nam rerum a. Sunt, consequuntur aperiam aliquam amet voluptatum, ducimus maxime quasi molestiae praesentium nisi fuga recusandae ex quis totam qui asperiores sint excepturi laboriosam!
-   Non repellendus nostrum voluptatum saepe amet id a eveniet ducimus quia ea dignissimos, autem libero, nihil necessitatibus porro quis esse harum iusto pariatur? Dicta unde rem debitis blanditiis assumenda fugit.
-   Natus minima inventore explicabo sequi, corporis corrupti optio ut amet nemo temporibus, rerum, molestiae esse non adipisci rem quae dolor itaque. Dolores debitis pariatur quam similique! Eius ipsa recusandae doloremque.
-   Sequi officiis vero, cum eaque fuga deserunt quia recusandae modi animi eum a labore sed quas veniam optio voluptatibus saepe perferendis omnis consequatur id accusantium! Animi delectus fugiat reiciendis sunt.
-   Pariatur fugit aliquam, ex unde, quibusdam facere sunt velit saepe dolores perspiciatis earum nisi nostrum quidem incidunt vitae quod odio. Deserunt laudantium vitae qui excepturi, perspiciatis libero illum optio doloremque.
-   Eius odio alias laborum quaerat incidunt ipsum dolore ipsam totam fugiat rem. Iure voluptates debitis fugiat accusantium obcaecati dolorum. Quod atque minus nobis itaque praesentium repellat a rem maiores iusto.
-   Modi ullam voluptates sed voluptatibus eligendi asperiores atque quas doloremque quia, incidunt numquam consequatur sunt dignissimos laudantium? Blanditiis ab sequi praesentium ut. Repudiandae quisquam recusandae eligendi perferendis dolor omnis consequuntur.</div>
-   <div class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit culpa placeat maiores labore incidunt. Fugiat, nihil reiciendis tempora nisi dolorem eveniet labore commodi neque voluptates molestias necessitatibus voluptatibus quod similique.
-   Velit facere inventore obcaecati necessitatibus quo expedita modi exercitationem quod, consequatur veniam pariatur eos labore numquam reiciendis harum corporis cum ab facilis enim quaerat. Dolorum porro sint nemo quis quidem!
-   Animi nostrum incidunt cumque magni nulla iste et aliquam tenetur officiis laudantium soluta assumenda sapiente doloribus facere dolorem harum, enim ad temporibus error, id possimus amet, obcaecati vero! Modi, deleniti.
-   Unde laudantium obcaecati beatae dolorum corporis minima, et iure eaque dolore excepturi ipsum molestias quam velit sed saepe amet non. Incidunt consectetur ratione perspiciatis unde eveniet? Dignissimos maiores sunt cumque!
-   Cupiditate blanditiis laboriosam nihil voluptatem placeat amet quisquam itaque minus quasi id odio ea, repudiandae error exercitationem. Laudantium quibusdam, reprehenderit eos assumenda sed earum dolores, nostrum aut cumque labore rerum.
-   Error modi, iusto alias commodi, perferendis quae delectus, eaque ratione non odit ducimus quidem ad sed! Provident nam nesciunt facere dicta laborum, cupiditate, asperiores vel dignissimos veritatis, suscipit modi aut.
-   Hic saepe illo quam ullam, deserunt ratione tempora asperiores, provident error veniam alias quis amet iusto unde commodi sint laborum consectetur in maiores minima nostrum. Esse quisquam omnis accusantium! Itaque.
-   Ratione enim cum quidem, expedita rem ea modi laudantium mollitia id labore! Tenetur dolor, reprehenderit deserunt aliquam facilis accusamus! Error laboriosam suscipit corrupti eveniet impedit laudantium, quo porro consequatur hic.
-   Laboriosam iure maxime, odit hic tenetur quod at sit dicta totam. Sunt ea similique qui saepe! Esse quod quibusdam adipisci vitae nam asperiores, sit, possimus accusantium repellat inventore sint ut!
-   Non culpa hic eius vel provident minima, magni temporibus. Incidunt illo, recusandae, doloremque natus aperiam iusto autem vitae mollitia laboriosam necessitatibus esse beatae sequi fuga officiis quia. Voluptas, incidunt veniam!</div>
+ <!-- Card section -->
+ <div class="container">
+      <div class="row mt-5">
+        <!-- Card 1 -->
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <div class="image-container">
+              <img class="card-img-top" src="../assets/img_bikers/biker1.jpg" alt="Card image cap">
+            </div>
+          </div>
+        </div>
 
+        <!-- Card 2 -->
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <div class="image-container">
+              <img class="card-img-top" src="../assets/img_bikers/biker4.jpg" alt="Card image cap">
+            </div>
+          </div>
+        </div>
 
+        <!-- Card 3 -->
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <div class="image-container">
+              <img class="card-img-top" src="../assets/img_bikers/biker3.jpg" alt="Card image cap">
+            </div>
+          </div>
+        </div>
 
-  </div>
- </div>
+        <!-- Card 4 -->
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <div class="image-container">
+              <img class="card-img-top" src="../assets/img_bikers/biker2.jpg" alt="Card image cap">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </section>
 </template>
 
@@ -73,9 +66,31 @@ export default {
 @use "bootstrap/scss/bootstrap";
 section{
    background-color: #FBFBFB;
-   
 }
 
+
+.card-img-top img{
+    transition: all 0.3 ease;
+    width: 100%;
+    height: auto;
+}
+
+.card:hover .card-img-top {
+  filter: brightness(50%);
+}
+.image-container {
+  position: relative; 
+}
+
+.image-container::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 33%; 
+  background-color: black;
+}
 
 
 </style>
