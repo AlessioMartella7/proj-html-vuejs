52a7a76e2b3c27b78fd421656b2f030e4a0df7d2
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-26 12:45:10 +0200

    added left column content

--- src/components/AppMainAboutUs.vue
+++ src/components/AppMainAboutUs.vue
@@ -11,14 +11,56 @@ export default {
 <template>
 <div class="container">
     <div class="row">
-        <div class="col-8"></div>
-        <div class="col-4"></div>
+
+        <!-- left side -->
+        <div class="col-8">
+            <img src=".././assets/img_aboutus/bike-player.png" alt="player-img">
+        </div>
+
+        <!-- right side -->
+        <div class="col-4 d-flex flex-column justify-content-center align-items-start">
+            <div class=" top-text">
+                <h3>Do you want to be a professional cyclist?</h3>
+                <p>We offer 6 courses of varying difficulty and beautiful scenary that cyclist of all levels can enjoy it. You will learn cycling from professionals
+                    with our component and experienced staff. You will have a lot of fun with our amazing spaces.
+                </p>
+             </div>
+             <div class="divider">
+                <img src=".././assets/img_aboutus/divider.jpg" alt="divider-img">
+             </div>
+
+
+             <div class="main-content">
+
+                <!-- professional team -->
+                <div class="team d-flex align-items-center">
+                    <img class="img-fluid" src="../assets/img_icons/f1-helmet-svgrepo-com.svg" alt="race-helmet">
+                    <div class="text">
+                        <h4 class="fw-bold">Professional Team</h4>
+                        <p>We make you professional by adding your sincerity as well as professionalism.</p>
+                    </div>
+                </div>
+
+                <!-- professional trainings -->
+                <div class="trainings d-flex align-items-center">
+                    <img class="img-fluid" src="../assets/img_icons/helmet-svgrepo-com.svg" alt="helmet">
+                    <div class="text">
+                        <h4 class="fw-bold">Professional trainings</h4>
+                        <p>We always ride with the best equipment, respecting nature and fun.</p>
+                    </div>
+                </div>
+             </div>
+
+        </div>
     </div>
 </div>
 </template>
 
 <style lang="scss" scoped>
 [class*="col"]{border: 1px dashed red;} //col higlight
-  
+
+.main-content img {
+    height: 80px;
+}
 
 </style>
