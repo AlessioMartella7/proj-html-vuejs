eecbead7a46c774c1d7bc9073a98eb2b9024d73f
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-27 10:15:38 +0200

    added icons ad formatted text to AppMainEvents

--- src/components/AppMainEvents.vue
+++ src/components/AppMainEvents.vue
@@ -22,18 +22,17 @@ export default {
 <div class="container">
   <div class="row ">
     <div v-for="event in events" class="col-4 my-3">
-          <div class="card" style="width: 25rem;">
-      <img :src="getImagePath(event.img)" class="card-img-top" :alt=event.race>
-        <div class="card-body pb-0">
-          <h5 class="card-title fw-bold ">{{ event.race }}</h5>
-        </div>
-        <font-awesome-icon :icon="['far', 'clock']" />
-        <font-awesome-icon :icon="['fas', 'location-dot']" />
-          <ul class="list-group list-group-flush border-0">
-            <li class="list-group-item border-0 fs-6">{{ event.date }}</li>
-            <li class="list-group-item border-0">{{ event.location }}</li>
-            <li class="list-group-item">{{ event.type }}</li>
-          </ul>
+      <div class="card h-100">
+          <img :src="getImagePath(event.img)" class="card-img-top" :alt="event.race">
+          <div class="card-body pb-0">
+            <h5 class="card-title fw-bold">{{ event.race }}</h5>
+            <div class="d-flex align-items-center mb-2">
+              <font-awesome-icon :icon="['far', 'clock']" class="me-2" /> <span>{{ event.date }}</span>
+            </div>
+            <div class="d-flex align-items-center mb-2">
+              <font-awesome-icon :icon="['fas', 'location-dot']" class="me-2" /> <span>{{ event.location }}</span>
+            </div>
+          </div>
       </div>
     </div>
   </div>
